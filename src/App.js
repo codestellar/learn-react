4005c480884ac703911ecf0b029086102878df8a
codestellar <gauravmadaan@outlook.com>
2021-07-29 19:32:43 +0530

    Created first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./style.css";
+import React, { Fragment } from 'react';
+import './style.css';
+import Header from './components/Header';
 
 export default function App() {
+
+  const name = 'Gaurav';
+  const x = 0;
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
+    <Fragment>
+      <Header/>
+      <h1>Hello {name}!</h1>
       <p>Start editing to see some magic happen :)</p>
-    </div>
+      {/* This is how you add comment and the following is the if else*/}
+      <p>{x == 0? 'Yay!': 'No'}</p>
+    </Fragment>
   );
 }
